e00c52154435e394859e7bb835f7695615102df9
ivy497 <ivanaw279@yahoo.com>
2026-03-09 20:41:58 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -55,16 +55,16 @@ console.log(bob.name, obj3.name);//Shelly, Smith
       ]);
 
 //function MUST be declared async
-// async function getData(url){
-//    try{
-//      let response = await fetch(url);//1. Send http request and get response
-//      let result = await response.json();//2. Get data from response
-//      drawTable(result);// 3. Do something with the data
-//   }catch(e){
-//       console.log(e);//catch and log any errors
-//   }
-// }
-// getData("https://api.openbrewerydb.org/v1/breweries")
+async function getData(url){
+   try{
+     let response = await fetch(url);//1. Send http request and get response
+     let result = await response.json();//2. Get data from response
+     drawTable(result);// 3. Do something with the data
+  }catch(e){
+      console.log(e);//catch and log any errors
+  }
+}
+getData("https://api.openbrewerydb.org/v1/breweries")
 // //get client data using navigator
 
 console.log(window.navigator.cookieEnabled);
